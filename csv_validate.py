fa3cac016ae1c20246bbe5781b10bcbd920aa6f7
Christopher Alcock <christopher.alcock@gmail.com>
2021-01-28 16:14:58 +0000

    code now supports receiving csv as its contents as well as its location, hopefully allowing it to be easily used in whatever environment it is needed in

--- csv_validate.py
+++ csv_validate.py
@@ -1,15 +1,21 @@
 import csv
+from io import StringIO
+
 
 class CsvValidityAssessment:
     """
     Details CSV validty
     """
-    def __init__(self, file_path, expected_column_count):
+    def __init__(
+        self,
+        file_path: 'str',
+        expected_column_count: 'int'
+    ):
         """
         Construct a new 'CsvValidityAssessment' object.
 
         :param file_path: The location of the csv in question
-        :param expected_column_count: The number of columns we're expecting 
+        :param expected_column_count: The number of columns we're expecting
             the csv to have
         :return: returns nothing
         """
@@ -20,30 +26,92 @@ class CsvValidityAssessment:
         self.first_problem_row_width = None
 
 
-def validate_csv(
-    file_path:'str',
-    expected_column_count:'int',
-    delimiter:'char',
-    quotechar:'char'='"') -> CsvValidityAssessment:
+def validate_csv_string(
+    csv_string: 'str',
+    file_path: 'str',
+    expected_column_count: 'int',
+    delimiter: 'str',
+    quotechar: 'str' = '"'
+) -> CsvValidityAssessment:
     """
-        Take details of csv and return assessment of its validity
+    Take string of csv data and return CsvValidityAssessment
 
-        :param file_path: The location of the csv in question
-        :param expected_column_count: The number of columns we're expecting
-            the csv to have
-        :param delimiter: character that separates the line of the csv into
-            its columns, typically a comma
-        :param quotechar: character that wraps a delimiter when you want to
-            use it without it separating a column in two, typically a double
-            quotation mark
-        :return: returns CsvValidityAssessment
+    :param csv_string: A string of csv data
+    :param file_path: The location of the csv in question, used here to pass
+        through to the CsvValidityAssessment to help action insights
+    :param expected_column_count: The number of columns we're expecting
+        the csv to have
+    :param delimiter: character that separates the line of the csv into
+        its columns, typically a comma
+    :param quotechar: character that wraps a delimiter when you want to
+        use it without it separating a column in two, typically a double
+        quotation mark
+    :return: returns CsvValidityAssessment
+    """
+    with StringIO(csv_string) as csv_file:
+        return validate_csv(
+            csv_file,
+            file_path,
+            expected_column_count,
+            delimiter,
+            quotechar)
+
+
+def validate_csv_file(
+    file_path: 'str',
+    expected_column_count: 'int',
+    delimiter: 'str',
+    quotechar: 'str' = '"'
+) -> CsvValidityAssessment:
+    """
+    Take string of csv file location and return CsvValidityAssessment
+
+    :param file_path: The location of the csv in question
+    :param expected_column_count: The number of columns we're expecting
+        the csv to have
+    :param delimiter: character that separates the line of the csv into
+        its columns, typically a comma
+    :param quotechar: character that wraps a delimiter when you want to
+        use it without it separating a column in two, typically a double
+        quotation mark
+    :return: returns CsvValidityAssessment
+    """
+    with open(file_path) as csv_file:
+        return validate_csv(
+            csv_file,
+            file_path,
+            expected_column_count,
+            delimiter,
+            quotechar)
+
+
+def validate_csv(
+    csv_file,
+    file_path: 'str',
+    expected_column_count: 'int',
+    delimiter: 'str',
+    quotechar: 'str' = '"'
+) -> CsvValidityAssessment:
+    """
+    Take csv file or file-like object and return assessment of its validity
+
+    :param csv_file: A file-like object such as the one returned by the
+        open method, or the io.StringIO method
+    :param file_path: The location of the csv in question
+    :param expected_column_count: The number of columns we're expecting
+        the csv to have
+    :param delimiter: character that separates the line of the csv into
+        its columns, typically a comma
+    :param quotechar: character that wraps a delimiter when you want to
+        use it without it separating a column in two, typically a double
+        quotation mark
+    :return: returns CsvValidityAssessment
     """
     assessment = CsvValidityAssessment(file_path, expected_column_count)
-    with open(file_path) as csvfile:
-        for row in csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar):
-            if len(row) != expected_column_count:
-                assessment.first_problem_row = row
-                assessment.csv_is_valid = False
-                assessment.first_problem_row_width = len(row)
-                break
+    for row in csv.reader(csv_file, delimiter=delimiter, quotechar=quotechar):
+        if len(row) != expected_column_count:
+            assessment.first_problem_row = row
+            assessment.csv_is_valid = False
+            assessment.first_problem_row_width = len(row)
+            break
     return assessment
--- csv_validate_test.py
+++ csv_validate_test.py
@@ -1,18 +1,54 @@
-import pytest
-from csv_validate import validate_csv
+from csv_validate import validate_csv_file, validate_csv_string
+
 
 def test_simple_valid_csv():
-    assert validate_csv("./test_data/simple_valid.csv",3,',').csv_is_valid == True
+    assert validate_csv_file(
+        "./test_data/simple_valid.csv",
+        3,
+        ','
+    ).csv_is_valid is True
+
+
+def test_simple_valid_csv_from_string():
+    csv_string = """Col1,Col2,Col3
+val1,val2,val3
+val4,val5,val6
+val7,val8,val9"""
+    assert validate_csv_string(
+        csv_string,
+        "simple_valid.csv",
+        3,
+        ','
+    ).csv_is_valid is True
+
 
 def test_non_quadralateral_invalid_csv():
-    assert validate_csv("./test_data/non_quadralateral_invalid.csv",2,',').csv_is_valid == False
+    assert validate_csv_file(
+        "./test_data/non_quadralateral_invalid.csv",
+        2,
+        ','
+    ).csv_is_valid is False
+
 
 def test_complex_escaping_valid_csv():
-    assert validate_csv("./test_data/complex_escaping_valid.csv",3,',').csv_is_valid
+    assert validate_csv_file(
+        "./test_data/complex_escaping_valid.csv",
+        3,
+        ','
+    ).csv_is_valid is True
+
 
 def test_complex_escaping_valid_csv_with_wrong_delimiter():
-    assert validate_csv("./test_data/complex_escaping_valid.csv",3,'|').csv_is_valid == False
+    assert validate_csv_file(
+        "./test_data/complex_escaping_valid.csv",
+        3,
+        '|'
+    ).csv_is_valid is False
+
 
 def test_quotes_strings_valid_csv():
-    assert validate_csv("./test_data/quotes_strings_valid.csv",3,',').csv_is_valid == True
-
+    assert validate_csv_file(
+        "./test_data/quotes_strings_valid.csv",
+        3,
+        ','
+    ).csv_is_valid is True
